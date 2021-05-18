0c27416413d5b48972cda665b50877cd1b6d964f
Jeremiah Ruiz <jruiz181@west-mec.org>
2021-05-18 14:30:25 -0700

    fr

--- numbers.js
+++ numbers.js
@@ -1,72 +1,206 @@
-//step 1 select objects on screen & define globals//
-const holes = document.querySelectorAll('.hole');
-const scoreboard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
-const countdownBoard = document.querySelector('.countdown');
-const startButton = document.querySelector('.startButton');
-let lastHole;
-let timeUp = false;
-let score = 0;
-let countdown;
-let timeLimit = 2000
-function pickRandomHole(holes) {
-    const randomHole = Math.floor(Math.random() * holes.length);
-    const hole = holes[randomHole];
-    if (hole === lastHole){
-        return(pickRandomHole(holes));
-    }
-    lastHole = hole;
-    return hole;
-}
-//step 2 add class 'up'//
-function popOut(){
-    const time = Math.random() * 1200 + 400;
-    const hole = pickRandomHole(holes);
-    hole.classList.add('up');
-    setTimeout(function(){
-        hole.classList.remove('up');
-        if (!timeUp) {
-            popOut();
-        }
-    }, time);
-}
-//step 3 dynamically change text content to countdown//
-function startGame() {
-    countdown = timeLimit/20;
-    scoreboard.textContent = 0;
-    scoreboard.style.display = 'block';
-    countdownBoard.textContent = countdown;
-    timeUp = false;
-    score = 0;
-    popOut();
-    setTimeout(function(){
-        timeUp = true;
-    }, timeLimit);
-    let startCountdown = setInterval(function(){
-        countdown -= 1;
-        countdownBoard.textContent = countdown;
-        if (countdown < 0){
-            countdown = 0;
-            clearInterval(startCountdown)
-            countdownBoard.textContent = 'Times Up!! Thanks for assaulting tiny yodas.';
-        }
-    }, 1000)
-}
-//step 4 adding event listeners//
-startButton.addEventListener('click', startGame);
 
-function whack(e){
-    score++;
-    this.style.backgroundImage = 'url("yoda2.png")';
-    this.style.pointerEvents = 'none';
-    setTimeout(() =>{
-        this.style.backgroundImage = 'url("yoda1.png")';
-        this.style.pointerEvents = 'all';
-    }, 800)
-    scoreboard.textContent = score;
+//-------------global variables--------------
+
+var first = (document.getElementById("first"));
+var second = (document.getElementById("second"));
+var third = (document.getElementById("third"));
+var fourth = (document.getElementById("fourth"));
+var fifth = (document.getElementById("fifth"));
+var sixth = (document.getElementById("sixth"));
+var output = (document.getElementById("output"));
+
+//--------Function order button----------------
+
+function order(){
+    num1= parseFloat(first.value);
+    num2= parseFloat(second.value);
+    num3 = parseFloat(third.value);
+    num4 = parseFloat(fourth.value);
+    num5 = parseFloat(fifth.value);
+    num6 = parseFloat(sixth.value);
+
+    var o1 = 0;
+    var o2 = 0;
+    var o3 = 0;
+    var o4 = 0;
+    var o5 = 0;
+    var o6 = 0;
+
+    o1 = Math.min(num1, num2, num3, num4, num5, num6);
+
+    if (o1 === num1){
+        num1 = num2;
+        num2 = num3;
+        num3 = num4;
+        num4 = num5;
+        num5 = num6;
+    }else if(o1 === num2){
+        num1 = num1;
+        num2 = num3;
+        num3 = num4;
+        num4 = num5;
+        num5 = num6;
+    }else if(o1 === num3){
+        num1 = num1;
+        num2 = num2;
+        num3 = num4;
+        num4 = num5;
+        num5 = num6;
+    }else if(o1 === num4){
+        num1 = num1;
+        num2 = num2;
+        num3 = num3;
+        num4 = num5;
+        num5 = num6;
+    }else if(o1 === num5){
+        num1 = num1;
+        num2 = num2;
+        num3 = num3;
+        num4 = num4;
+        num5 = num6;
+    }else if(o1 === num6){
+        num1 = num1;
+        num2 = num2;
+        num3 = num3;
+        num4 = num4;
+        num5 = num5;
+    }
+
+    o2 = Math.min(num1, num2, num3, num4, num5);
+
+    if (o2 === num1){
+        num1 = num2;
+        num2 = num3;
+        num3 = num4;
+        num4 = num5;
+    }else if(o2 === num2){
+        num1 = num1;
+        num2 = num3;
+        num3 = num4;
+        num4 = num5;
+    }else if(o2 === num3){
+        num1 = num1;
+        num2 = num2;
+        num3 = num4;
+        num4 = num5;
+    }else if(o2 === num4){
+        num1 = num1;
+        num2 = num2;
+        num3 = num3;
+        num4 = num5;
+    }else if(o2 === num5){
+        num1 = num1;
+        num2 = num2;
+        num3 = num3;
+        num4 = num4;
+    }
+
+    o3 = Math.min(num1,num2,num3,num4);
+
+    if (o3 === num1){
+        num1 = num2;
+        num2 = num3;
+        num3 = num4;
+        num4 = num5;
+    }else if(o3 === num2){
+        num1 = num1;
+        num2 = num3;
+        num3 = num4;
+        num4 = num5;
+    }else if(o3 === num3){
+        num1 = num1;
+        num2 = num2;
+        num3 = num4;
+        num4 = num5;
+    }else if(o3 === num4){
+        num1 = num1;
+        num2 = num2;
+        num3 = num3;
+        num4 = num5;
+    }
+
+    o4 = Math.min(num1,num2,num3);
+
+    if (o4 === num1){
+        num1 = num2;
+        num2 = num3;
+        num3 = num4;
+    }else if(o4 === num2){
+        num1 = num1;
+        num2 = num3;
+        num3 = num4;
+    }else if(o4 === num3){
+        num1 = num1;
+        num2 = num2;
+        num3 = num4;
+    }
+
+    o5 = Math.min(num1,num2);
+    if (o5 === num1){
+        num1 = num2;
+        num2 = num2;
+
+    }else if(o5 === num2){
+        num1 = num1;
+        num2 = num2;
+
+    }
+
+    o6 = Math.min(num1);
+
+     output.innerHTML = o1 + " , " + o2 + " , " + o3 +" , " + o4 +" , " + o5 +" , " + o6
 }
-//step 5 keep track of score with forEach//
-moles.forEach(mole => mole.addEventListener('click', whack));
+
+//--------------function mean-------------------
+
+function mean(){
+    num1= parseFloat(first.value);
+    num2= parseFloat(second.value);
+    num3 = parseFloat(third.value);
+    num4 = parseFloat(fourth.value);
+    num5 = parseFloat(fifth.value);
+    num6 = parseFloat(sixth.value);
+
+    var mean = ( num1 + num2 + num3 + num4 + num5 + num6 ) / 6;
+    console.log(mean);
+    output.innerHTML = mean;
+    
+    return true
+}
+
+//---------------Function range--------------------
+
+function range(){
+    num1= parseFloat(first.value);
+    num2= parseFloat(second.value);
+    num3 = parseFloat(third.value);
+    num4 = parseFloat(fourth.value);
+    num5 = parseFloat(fifth.value);
+    num6 = parseFloat(sixth.value);
+
+    var large = Math.max(num1, num2, num3, num4, num5, num6);
+    var small = Math.min(num1, num2, num3, num4, num5, num6);
+
+    output.innerHTML = large - small;
+    return true
+}
+
+//-------------function smallest number---------------
+
+function small(){
+    num1= parseFloat(first.value);
+    num2= parseFloat(second.value);
+    num3 = parseFloat(third.value);
+    num4 = parseFloat(fourth.value);
+    num5 = parseFloat(fifth.value);
+    num6 = parseFloat(sixth.value);
+    
+    var small = Math.min(num1, num2, num3, num4, num5, num6);
+
+    output.innerHTML = small;
+
+    return true
+}
 
 //----------------function largest number-----------------
 
@@ -94,6 +228,7 @@ function middle(){
     num4 = parseFloat(fourth.value);
     num5 = parseFloat(fifth.value);
     num6 = parseFloat(sixth.value);
+
 
 }
 
